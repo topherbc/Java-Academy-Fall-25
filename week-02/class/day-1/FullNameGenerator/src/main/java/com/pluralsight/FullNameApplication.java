a9a84136d22840417a5914e490f3d3d4b8699afb
Topher C <topherbyron@duck.com>
2025-09-29 11:16:37 -0400

    day 1 week 2 mid day

---
+++ week-02/class/day-1/FullNameGenerator/src/main/java/com/pluralsight/FullNameApplication.java
@@ -0,0 +1,37 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class FullNameApplication {
+    public static void main(String[] args) {
+
+
+        /**
+         * Expected Output:
+         *
+         * Please enter your name
+         * First name: (user enters first name, optional)
+         * Middle name: (user enters middle name, optional)
+         * Last name:  (user enters last name, optional)
+         * Suffix: (user enters suffix, optional)
+         * Full name: (concatenated full name)
+         */
+        Scanner myScanner = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Please enter your name");
+        System.out.print("First name: ");
+        String firstName = myScanner.nextLine();
+
+        System.out.print("Middle name: ");
+        String middleName = myScanner.nextLine();
+
+        System.out.print("Last name: ");
+        String lastName = myScanner.nextLine();
+
+        System.out.print("Suffix: ");
+        String suffix = myScanner.nextLine();
+
+        System.out.printf("Full name: %s %s %s %s\n", firstName, middleName, lastName, suffix);
+//        System.out.printf("Full name: " + firstName + " " + middleName + " " + lastName + " " + suffix);
+    }
+
+}
