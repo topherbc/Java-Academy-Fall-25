c732e79c2ba069b3013741756c2d48aa13a53e37
Topher C <topherbyron@duck.com>
2025-10-08 11:32:02 -0400

    week 3 midday 3

---
+++ week-03/class/day-3/SearchInventory/src/main/java/com/pluralsight/StoreApp.java
@@ -0,0 +1,111 @@
+package com.pluralsight;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StoreApp {
+    public static void main(String[] args) {
+        System.out.println("---Welcome to Super Duper's Product Inventory System---\n");
+
+        run();
+    }
+
+    public static void run() {
+        while(true) {
+            menu();
+            menuSelector();
+        }
+    }
+
+    public static void menu() {
+        System.out.println("What do you want to do?\n" +
+                "  1- List all products\n" +
+                "  2- Lookup a product by its id\n" +
+                "  3- Find all products within a price range\n" +
+                "  4- Add a new product\n" +
+                "  5- Quit the application\n");
+    }
+
+    public static void menuSelector() {
+        ArrayList<Product> inventory = getInventory();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int option = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (option) {
+            case 1:
+                System.out.println();
+                viewAll(inventory);
+                break;
+            case 2:
+                //searchProductByID(inventory, id)
+                break;
+            case 3:
+                //searchProductByPriceRange(inventory, startRange, endRange)
+                break;
+            case 4:
+                //inventory = addProduct(inventory)
+                break;
+            case 5:
+                scanner.close();
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Incorrect option entered, try again");
+        }
+
+        System.out.println("\nPress ENTER to continue...\n");
+        scanner.nextLine();
+
+    }
+
+    public static ArrayList<Product> getInventory() {
+        ArrayList<Product> inventory = new ArrayList<Product>();
+        // Unused Manual Product Creation
+        //            inventory.add(new Product(1234, "Cabin Air Filter", 17.00f));
+        //            inventory.add(new Product(1235, "Engine Oil 5W-30", 24.99f));
+        //            inventory.add(new Product(1236, "Brake Pads Set", 45.50f));
+        //            inventory.add(new Product(1237, "Windshield Wiper Blades", 19.95f));
+        //            inventory.add(new Product(1238, "Car Battery", 89.99f));
+        //            inventory.add(new Product(1239, "Spark Plugs 4-Pack", 32.75f));
+
+
+        try {
+            BufferedReader bufReader = new BufferedReader(new FileReader("src/main/resources/inventory.csv"));
+
+            String input;
+
+            while((input = bufReader.readLine()) != null) {
+                String[] tokens = input.split("\\|");
+
+                int id = Integer.parseInt(tokens[0]);
+                String name = tokens[1];
+                float price = Float.parseFloat(tokens[2]);
+
+                inventory.add(new Product(id, name, price));
+            }
+
+            bufReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return inventory;
+    }
+
+    public static void viewAll(ArrayList<Product> inventory) {
+        for (Product inventoryItem : inventory) {
+            displayProduct(inventoryItem);
+        }
+    }
+
+    public static void displayProduct(Product p) {
+        System.out.printf("Product: %d\n  Name: %s\n  Price: $%.2f\n\n", p.getId(), p.getName(), p.getPrice());
+    }
+}
---
+++ week-03/class/day-3/day-3-objectives.md
@@ -0,0 +1,43 @@
+What did you learn yesterday?
+
+1. BufferedReader
+   1. .ready() - returns true if the next line is readable
+   2. .close() - closes and saves 
+2. FileReader
+3. BufferedWriter
+   1. .write() - writes to a file
+   2.  .close() - closes and saves 
+4. FileWriter
+5. Try and Catch
+   1. try some code
+   2. catch the exception
+6. IOException
+7. I/O - Inputs and Outputs
+8. .json - JavaScript Object Notation
+9. .csv - comma separated values
+10. Stack Trace - Where the error occurs, and the trace of all those actions that failed
+11. Relative Path - Path is based current location
+12. Absolute Path - Full Path
+13. import java.something.*, * gets all packages from 'something'
+14. String.format() - formats string, using formatter rules
+15. Seeking unconformability enables learning
+
+Today's Objectives
+
+1. Collections
+   1. ArrayLists
+   2. HashMaps
+
+Agenda Today
+
+1. Recap
+2. ArrayLists lesson
+3. You build arraylist app
+4. Break
+5. You build app
+6. Lunch
+7. HashMaps Lesson
+8. You build hashmap app
+9. Break
+10. We build hashmap app
+11. Recap and Q&A
